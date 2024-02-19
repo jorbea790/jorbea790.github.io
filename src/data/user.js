39ef17532f0bbb5a85245f849fbf2d5a71b1222e
jorbea790 <jorbea790@gmail.com>
2024-02-19 11:44:04 -0500

    Removed some stock images

--- src/data/user.js
+++ src/data/user.js
@@ -1,8 +1,8 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "Portfolio by Jordan Bean",
+		name: "Jordan Bean",
+		email: "jorbea790@gmail.com",
 		logo: "../logo.png",
 	},
 
@@ -10,13 +10,13 @@ const INFO = {
 		twitter: "https://twitter.com/",
 		github: "https://github.com/",
 		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
+		instagram: "www.linkedin.com/in/jorbea790",
 		stackoverflow: "https://stackoverflow.com/",
 		facebook: "https://facebook.com/",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Full-stack engineer and aspiring machine learning practitioner.",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
